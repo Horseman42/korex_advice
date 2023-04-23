9edf9110cb956df17861f1e114eacc9009fa0470
unknown <babalolaridwan4real@gmail.com>
2023-04-23 08:34:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) =>{
+        const {advice} = response.data.slip;
+
+        setAdvice(advice);
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+  }
+
+  function getAdvice (){
+    fetchAdvice(); 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='app'>
+      <div className='card'>
+        <h2 className='heading'>{advice}</h2>  
+        <button className='button' onClick={getAdvice}><span>GIVE ME ADVICE!</span></button>
+      </div>
+    </div>  
+    
+  );  
+}  
 
-export default App;
+ export default App;
